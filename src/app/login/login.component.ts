f54323298b837d51482a1ef08b97d8a090bbc749
Sreejish KS <Sreejish KS>
2016-10-19 20:18:54 +1300

    service added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { LoginService }  from '../service/login.service';
 
 
 @Component({
   selector: 'app-login',
+  providers: [LoginService],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  getData: String;
+  email:String;
+  password:String;
 
-  constructor() {}
+  constructor(private _loginservice : LoginService) {}
 
   ngOnInit() {
+
   }
 
-  data = {};
   loginFormSubmit(form: any): void {  
+    let email = 'srijishks@gmail.com';
+    let password = 'qwer';
+    this._loginservice.makeLogin(email, password )
+    .subscribe(
+      data => this.getData = data,
+      error => alert('error'),
+      () => console.log("just finished")
+      );
     console.log('you submitted value:', form);  
     
   }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/Rx';
+
 
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  constructor(private http:Http) { }
+  	makeLogin(email:String,  password:String){
+  		let body = {email: email, password:password }
+  		// let bodyString = JSON.stringify(body); // Stringify payload
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers }); // Create a request option
 
+        return this.http.post("http://localhost/note3/login", body, options) // ...using post request
+                         .map((res:Response) => res.json()) ;
+
+  		
+  	}
 }
