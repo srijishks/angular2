a84d906286a5beb57e06ac3e7e2432db6a42342c
Sreejish KS <Sreejish KS>
2016-11-08 11:17:21 +1300

    Rich Editor added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import { MainmenuComponent } from './mainmenu/mainmenu.component';
 
+declare var Richeditor: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,6 +12,26 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  	var editor = new Richeditor('#editor', {
+    uploader: {
+        url: '../data/upload.php?action=upload',
+        process: function (resp) {
+            resp.baseurl = './data/user/files/';
+            return resp;
+        }
+    },
+    filebrowser: {
+        ajax: {
+            url: '../data/upload.php',
+            process: function (resp) {
+                resp.baseurl = './data/user/files/';
+                return resp;
+            },
+       	 }
+    	}
+	});
+
   }
 
 }
